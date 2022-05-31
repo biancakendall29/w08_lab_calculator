edc988893fd38d2215a0a6db7de8bc66039ea275
Bianca Kendall <[D)>
2022-05-31 15:49:58 +0200

    implemented subtract, multiply, divide and modulus

--- calculator.js
+++ calculator.js
@@ -1,14 +1,24 @@
+// Define the missing functions.
+// odd and even should take a single number as an argument and return a boolean to say if the number is odd/even or not.
+// The other functions should take two numbers as arguments and return the result of the appropriate operation, eg. sum takes two numbers and returns their sum
+// sum is already defined as an example
+// You can use named, anonymous or arrow functions
+
 const sum = function(a, b){
     return a + b;
 };
 
-const subtract = null;
+const subtract = (a, b) => Math.abs(a - b);
+console.log(subtract(2, 3));
 
-const multiply = null;
+const multiply = (a, b) => (a * b);
+console.log(multiply(2, 3));
 
-const divide = null;
+const divide = (a, b) => (a / b);
+console.log(divide(6, 3));
 
-const modulus = null;
+const modulus = (a, b) => (a % b);
+console.log(modulus(6, 5));
 
 const even = null;
 
