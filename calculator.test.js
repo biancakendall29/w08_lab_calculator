c49fa440c0e0e7219803fd7b3794181f11310dca
Bianca Kendall <[D)>
2022-05-31 17:18:29 +0200

    added 2 mvp tests each for divide and modulus

--- calculator.test.js
+++ calculator.test.js
@@ -43,32 +43,56 @@ describe('subtract', () => {
   test('can subtract two small positive numbers', () => {
     expected = 3;
     actual = subtract(5, 2);
+    expect(actual).toBe(expected);
   });
 
   test('can subtract two large positive numbers', () => {
     expected = 44546;
     actual = subtract(123456, 78910);
+    expect(actual).toBe(expected);
   });
 });
 
 describe('multiply', () => {
   test('can multiply two small positive numbers', () => {
     expected = 10;
-    actual = subtract(2, 5);
+    actual = multiply(2, 5);
+    expect(actual).toBe(expected);
   });
 
   test('can multiply two large positive numbers', () => {
     expected = 7006652;
-    actual = subtract(1234, 5678);
+    actual = multiply(1234, 5678);
+    expect(actual).toBe(expected);
   });
 });
 
 describe('divide', () => {
+  test('can divide two small positive numbers', () => {
+    expected = 5;
+    actual = divide(15, 3);
+    expect(actual).toBe(expected);
+  });
 
+  test('can divide a large positive number by a small one', () => {
+    expected = 20576;
+    actual = divide(123456, 6);
+    expect(actual).toBe(expected);
+  });
 });
 
 describe('modulus', () => {
+  test('can get the modulus of two small positive numbers', () => {
+    expected = 3;
+    actual = modulus(15, 4);
+    expect(actual).toBe(expected);
+  });
 
+  test('can get the modulus between two large numbers', () => {
+    expected = 372;
+    actual = modulus(123456, 789);
+    expect(actual).toBe(expected);
+  });
 });
 
 describe('even', () => {
