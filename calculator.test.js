f86a16e0d2919fd9aacc3118a80061f666720bd2
Bianca Kendall <[D)>
2022-05-31 16:15:19 +0200

    added mvp sum test and ran with passing result

--- calculator.test.js
+++ calculator.test.js
@@ -1,3 +1,6 @@
+// Complete the missing tests for sum. Delete .skip from the definitions in order to let them run.
+// Write tests for the other functions you have defined. We want to see at least two tests for each function.
+
 const {
   sum,
   subtract,
@@ -16,16 +19,22 @@ describe('sum', () => {
     expect(actual).toBe(expected);
   });
 
-  test.skip('can add two large positive numbers', () => { 
-    
+  test('can add two large positive numbers', () => { 
+    expected = 202366;
+    actual = sum(123456, 78910);
+    expect(actual).toBe(expected);  
   });
 
   test.skip('can add two negative numbers', () => {
-    
+    expected = 3;
+    actual = sum(-2, 5);
+    expect(actual).toBe(expected);
   });
 
   test.skip('can add zero', () => {
-    
+    expected = 7;
+    actual = sum(0, 7);
+    expect(actual).toBe(expected);
   });
 
 });
